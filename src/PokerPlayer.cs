29a9731158692afdcae4f5e29e4ee029e5383b1b
Szentivanyi Matyas <mszentivanyi@anycode.hu>
2017-04-22 10:28:34 +0200

    return some bet

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -4,12 +4,14 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+		public static readonly string VERSION = "Pogácsa poker bot";
 
 		public static int BetRequest(JObject gameState)
 		{
+            int currentPlayer = (int)gameState["in_action"];
+
 			//TODO: Use this method to return the value You want to bet
-			return 0;
+			return 100;
 		}
 
 		public static void ShowDown(JObject gameState)
