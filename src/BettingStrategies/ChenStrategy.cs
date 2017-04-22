17804d2c14326a1ca958503a9e6c44d2ac225504
Botond Botos <botond.botos@gmail.com>
2017-04-22 16:28:13 +0200

    Chen strategy added.

---
+++ src/BettingStrategies/ChenStrategy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Nancy.Simple.Model;
+
+namespace Nancy.Simple.BettingStrategies
+{
+    public class ChenStrategy : IBettingStrategy
+    {
+        public int PlaceBet(GameState gameState)
+        {
+            var rankingService = new RankingService();
+            int bet = 0;
+
+            try
+            {
+                var myself = gameState.players[gameState.in_action];
+                var handCards = gameState.players[gameState.in_action].hole_cards;
+
+                int chenValue = rankingService.GetChenRanking(handCards);
+
+                int callAmount = gameState.current_buy_in - myself.bet + gameState.minimum_raise;
+                int allInAmount = myself.stack;
+                int activePlayerCount = gameState.players.Count(player => player.status == "active");
+
+                bool inPreflop = gameState.community_cards.Count == 0;
+
+                if (inPreflop)
+                {
+                    if (chenValue >= 7)
+                    {
+                        return Math.Min(callAmount, (int)allInAmount / 2);
+                    }
+
+                    return 0;
+                }
+
+                bet = 100 + new Random().Next(10, 100);
+
+                return bet;
+
+            }
+            catch (Exception e)
+            {
+                //Console.Error.WriteLine("raw game state: {0}", rawGameState.ToString());
+
+                Console.Error.WriteLine("Error converting JSON {0}", e.Message);
+                Console.Error.WriteLine("Stack trace: {0}", e.StackTrace);
+            }
+
+            return 0;
+        }
+    }
+}
--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Nancy.Simple.Model;
+﻿using Nancy.Simple.BettingStrategies;
+using Nancy.Simple.Model;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Linq;
@@ -11,52 +12,12 @@ namespace Nancy.Simple
 
 		public static int BetRequest(JObject rawGameState)
 		{
-            var rankingService = new RankingService();
-            int bet = 0;
-            
-            try {
+            IBettingStrategy strategy = new ChenStrategy();
 
-                GameState gameState = rawGameState.ToObject<GameState>();
+            GameState gameState = rawGameState.ToObject<GameState>();
 
-                var myself = gameState.players[gameState.in_action];
-                var handCards = gameState.players[gameState.in_action].hole_cards;
-
-                int chenValue = rankingService.GetChenRanking(handCards);
-
-                int callAmount = gameState.current_buy_in - myself.bet + gameState.minimum_raise;
-                int allInAmount = myself.stack;
-                int activePlayerCount = gameState.players.Count(player => player.status == "active");
-
-                bool inPreflop = gameState.community_cards.Count == 0;
-
-                if (inPreflop)
-                {
-                    if (chenValue >= 7)
-                    {
-                        return Math.Min(callAmount, (int)allInAmount / 2);
-                    }
-
-                    return 0;
-                }
-
-                bet = 100 + new Random().Next(10, 100);
-
-                return bet;
-                
-            }
-            catch (Exception e)
-            {
-                //Console.Error.WriteLine("raw game state: {0}", rawGameState.ToString());
-
-                Console.Error.WriteLine("Error converting JSON {0}", e.Message);
-                Console.Error.WriteLine("Stack trace: {0}", e.StackTrace);
-            }
-
-            return 0;
-
-
+            return strategy.PlaceBet(gameState);
 		}
-
 
 		public static void ShowDown(JObject gameState)  
 		{
