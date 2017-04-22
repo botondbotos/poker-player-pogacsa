2d4ed8850cc91fb58b2e09e39977af7d5c0da327
Szentivanyi Matyas <mszentivanyi@anycode.hu>
2017-04-22 10:58:41 +0200

    add ranking service

---
+++ src/RankingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Simple
+{
+    class RankingService
+    {
+        public static int Rank()
+        {
+            return 0;
+        }
+
+        
+    }
+}
