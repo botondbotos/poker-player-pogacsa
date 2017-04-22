061fd2400d0037bceb3c1ea5e8c967d5e938f7f6
Szentivanyi Matyas <mszentivanyi@anycode.hu>
2017-04-22 11:48:29 +0200

    add random

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -10,14 +10,17 @@ namespace Nancy.Simple
 
 		public static int BetRequest(JObject gameState)
 		{
+            var kocka = new Random();
             //int myPlayerIndex = (int)gameState["in_action"];
             //int myPlayer = (int)gameState["players"][myPlayerIndex];
 
             //Console.WriteLine("Teszt Elek");
+            var cardValue = RankingService.Rank();
 
 			//TODO: Use this method to return the value You want to bet
-			return 100;
+			return 100 + kocka.Next(10, 100);
 		}
+
 
 		public static void ShowDown(JObject gameState)
 		{
